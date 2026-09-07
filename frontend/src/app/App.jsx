8ef65337f2f10267388e26b2806d77e74299c183
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-09-07 22:58:54 +0530

    commit

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,2 +1,26 @@
+import "./App.css"
+import Editor from '@monaco-editor/react';
+function App() {
+    return (
+        <main className="h-screen w-full bg-black flex gap-5 p-4 ">
+            <aside className="h-full w-1/4 bg-[#55100D] rounded-xl">
+
+            </aside>
+            <section className="w-3/4 bg-[#1A0706] rounded-xl">
+                <Editor
+                    height="100%"
+                    defaultLanguage="javascript"
+                    defaultValue="//some comment"
+                    theme="vs-dark" />
+
+            </section>
+
+        </main>
+
+    )
+
+}
 
 
+
+export default App
